84900a2bc2f6b5e9c713d8a26de708b60497d550
Xiao <xiaoboost@163.com>
2017-04-06 23:01:41 +0800

    添加stylus库

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,12 @@ export default {
 };
 </script>
 
-<style>
-#app {
+<style lang="stylus">
+#app
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
 </style>
