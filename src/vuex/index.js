ab5108370ab6c6b9b9714ff59943bd0e93d716f9
Xiao <xiaoboost@163.com>
2017-04-10 23:11:28 +0800

    全局标准输入验证

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -3,11 +3,24 @@ import Vuex from 'Vuex';
 
 Vue.use(Vuex);
 
-//SVG的命名空间
+// SVG的命名空间
 const SVG_NS = 'http://www.w3.org/2000/svg';
+// 科学计数法匹配正则
+const NUM_REG = /^\d+(?:\.\d+)?$|^\d+?(?:\.\d+)?[eE]-?\d+$|^\d+(?:\.\d+)?[puμnmkMG]$/;
 
 export default new Vuex.Store({
     state: {
-        SVG_NS
+        SVG_NS,
+        NUM_REG,
+        END_TIME: 0,
+        STEP_TIME: 0
+    },
+    mutations: {
+        SET_END_TIME(state, time) {
+            state.END_TIME = time;
+        },
+        SET_STEP_TIME(state, time) {
+            state.STEP_TIME = time;
+        }
     }
 });
