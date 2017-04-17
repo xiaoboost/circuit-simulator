4355a7253d05abdd2ca4827b7df5e64d0fdaea92
Xiao <xiaoboost@163.com>
2017-04-17 22:53:20 +0800

    变更point构造函数

--- src/libraries/point.js
+++ src/libraries/point.js
@@ -1,16 +1,12 @@
 //点和向量
 class Point {
     constructor(a, b) {
-        if (a instanceof Point) {
-            // 复制
-            this[0] = a[0];
-            this[1] = a[1];
-        } else if (isPoint(a) && isPoint(b)) {
+        if (isPoint(a) && isPoint(b)) {
             // 输入是两个点，当作向量处理
             this[0] = b[0] - a[0];
             this[1] = b[1] - a[1];
-        } else if (isPoint(a)) {
-            // 输入是点
+        } else if (isPoint(a) && b === undefined) {
+            // 输入一个点
             this[0] = a[0];
             this[1] = a[1];
         }
@@ -20,10 +16,10 @@ class Point {
 
 function isPoint(a) {
     return (
-        a instanceof Object &&
         a instanceof Point ||
+        (a instanceof Object &&
         typeof a[0] === 'number' &&
-        typeof a[1] === 'number'
+        typeof a[1] === 'number')
     );
 }
 function $P(a, b) {
