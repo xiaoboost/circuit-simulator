1567af5543a6a2977573bf3f496fa49854c57df2
Xiao <xiaoboost@163.com>
2017-04-22 23:23:39 +0800

    绘图界面缩放

--- src/components/DrawingMain/Schematic.vue
+++ src/components/DrawingMain/Schematic.vue
@@ -1,19 +1,73 @@
 <template>
-<div class="drawing-main">
-    <svg height="100%" width="100%" version="1.1" :xmlns="SVG_NS">
-        <g id="area-of-parts">
+<section class="drawing-main" :style="background">
+    <svg height="100%" width="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <g :transform="`translate(${position.join(',')}) scale(${zoom})`">
         </g>
     </svg>
-</div>
+</section>
 </template>
 
 <script>
+import part from '@/components/ElectronicPart';
+import line from '@/components/ElectronicLine';
+
+import { $P } from '@/libraries/point';
+import { EventControler } from './EventControler';
+
 export default {
     name: 'DrawingMain',
+    // components: { part, line },
     data() {
         return {
-            SVG_NS: this.$store.state.SVG_NS
+            zoom: 1,
+            position: $P(0, 0)
         };
+    },
+    computed: {
+        // 网格背景图片
+        background() {
+            const bias = [
+                this.position[0] % (this.zoom * 20),
+                this.position[1] % (this.zoom * 20)
+            ];
+
+            return {
+                'background-size': `${this.zoom * 20}px`,
+                'background-position': `${bias[0]}px ${bias[1]}px`
+            };
+        }
+    },
+    methods: {
+        mousewheel(e) {
+            const mousePosition = $P(event.pageX, event.pageY);
+            let size = this.zoom * 20;
+
+            if (event.deltaY > 0) {
+                size -= 5;
+            } else if (event.deltaY < 0) {
+                size += 5;
+            }
+
+            if (size < 20) {
+                size = 20;
+                return (true);
+            }
+            if (size > 80) {
+                size = 80;
+                return (true);
+            }
+
+            this.position = this.position
+                .add(-1, mousePosition)
+                .mul(size / this.zoom / 20)
+                .add(mousePosition)
+                .round(1);
+
+            this.zoom = size / 20;
+        }
+    },
+    mounted() {
+        this.$el.addEventListener('mousewheel', (e) => this.mousewheel(e));
     }
 };
 </script>
