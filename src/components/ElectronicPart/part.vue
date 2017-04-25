bf3ad6a2d05bb1bdae557c3fde74360f6783a668
Xiao <xiaoboost@163.com>
2017-04-26 00:19:11 +0800

    器件引脚和文本初步

--- src/components/ElectronicPart/part.vue
+++ src/components/ElectronicPart/part.vue
@@ -1,6 +1,20 @@
 <template>
 <g class="part" :transform="`matrix(${rotate.join()},${position.join()})`">
-    器件
+    
+    <g 
+        v-for="point in points"
+        :class="point.class"
+        :transform="`translate(${point.position.join()})`">
+        <circle></circle>
+        <rect x="-9" y="-9" width="18" height="18"></rect>
+    </g>
+    <text
+        class="features-text"
+        transform="matrix(1,0,0,1,20,-2)">
+        <tspan dx="0" dy="0">{{texts.label}}</tspan>
+        <tspan>{{texts.sub}}</tspan>
+        <tspan v-for="param in texts.params" dy="16">{{param.text}}</tspan>
+    </text>
 </g>
 </template>
 
@@ -21,11 +35,52 @@ export default {
             id: '',
             type: '',
             params: [],
+            connect: [],
             rotate: [[1, 0], [0, 1]],
             position: [500000, 500000],
 
             shape: {}
         };
+    },
+    computed: {
+        points() {
+            function product(a, b) {
+                return [
+                    a[0] * b.get(0, 0) + a[1] * b.get(1, 0),
+                    a[0] * b.get(0, 1) + a[1] * b.get(1, 1)
+                ];
+            }
+            return this.shape.points.map((point, i) => {
+                return {
+                    position: product(point.position, this.rotate),
+                    direction: product(point.direction, this.rotate),
+                    class: [
+                        'part-point',
+                        {
+                            'point-open': !this.connect[i],
+                            'point-close': !!this.connect[i]
+                        }
+                    ]
+                };
+            });
+        },
+        texts() {
+            const id = this.id.split('_'),
+                params = this.shape.text
+                    .filter((n) => !n.hidden)
+                    .map((n, i) => {
+                        return {
+                            position: [],
+                            text: this.params[i] + n.unit
+                        };
+                    });
+
+            return {
+                label: id[0],
+                sub: id[1],
+                params
+            };
+        }
     },
     methods: {
         update() {
@@ -34,6 +89,7 @@ export default {
                 type: this.type,
                 rotate: this.rotate,
                 params: this.params,
+                connect: this.connect,
                 position: this.position
             });
         },
@@ -60,7 +116,7 @@ export default {
             });
         }
     },
-    mounted() {
+    created() {
         // 展开数据
         Object.assign(this, this.value);
         // 外观属性
@@ -68,7 +124,11 @@ export default {
         // 将旋转矩阵以及坐标实例化
         this.rotate = $M(this.rotate);
         this.position = $P(this.position);
-        // 如果坐标为无限，说明是新建器件
+        // 初始化后更新数据
+        this.update();
+    },
+    mounted() {
+        // 如果坐标为初始值，说明是新建器件
         if (this.position[0] === 500000) {
             this.setNewEvevt();
         }
