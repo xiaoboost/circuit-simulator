a7d595b39b31899320aed3506d470d4caac37e98
Xiao <xiaoboost@163.com>
2017-05-03 23:50:53 +0800

    加载页面单元测试完毕

--- test/unit/specs/start-loading.spec.js
+++ test/unit/specs/start-loading.spec.js
@@ -5,12 +5,20 @@ describe('start-loading.vue', () => {
     let vm;
     afterEach(() => destroyVM(vm));
 
-    it('create and show', () => {
+    it('Loading page, it should be showed', () => {
         vm = createTest(StartLoading, { show: true }, true);
 
         const text = vm.$el.querySelectorAll('h3');
         expect(text.length).to.be.equal(2);
         expect(text[0].textContent).to.be.equal('加载中……');
         expect(text[1].textContent).to.be.equal('绘制草图·模拟');
+    });
+    it('Page loading finished, it should be closed', (done) => {
+        vm = createTest(StartLoading, { show: false }, true);
+        // loading界面消失时有 200ms 的动画，所以这里设置等待 300ms
+        setTimeout(() => {
+            expect(vm.$el.childNodes.length).to.be.equal(0);
+            done();
+        }, 300);
     });
 });
