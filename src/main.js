8a6fb8ebf76b41fd553cdb9120f03745228b37ce
Xiao <xiaoboost@163.com>
2017-08-07 23:48:04 +0800

    变更调试初始化方式

--- src/main.js
+++ src/main.js
@@ -17,10 +17,10 @@ new Vue({
     /* no-build */
     mounted() {
         const area = document.querySelector('.drawing-main svg g'),
-            Compo = Vue.extend(require('@/components/map-debugger').default);
+            Compo = require('@/libraries/debugger').default;
 
         // 调试组件独立于 app，挂在全局变量上
-        window.$debug = new Compo().$mount();
+        window.$debug = new Compo();
         area.appendChild(window.$debug.$el);
     },
     /* no-build */
