34eec9d8d223d03daa8c65ec76c101e850bd7278
Xiao <xiaoboost@163.com>
2017-09-03 19:34:50 +0800

    调试时生成 html 文件时需要加入版本号

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -2,6 +2,7 @@ const utils = require('./utils'),
     webpack = require('webpack'),
     config = require('../config'),
     merge = require('webpack-merge'),
+    version = utils.createVersionTag(),
     baseWebpackConfig = require('./webpack.base.conf'),
     HtmlWebpackPlugin = require('html-webpack-plugin'),
     FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
@@ -25,16 +26,17 @@ module.exports = merge(baseWebpackConfig, {
         new webpack.DefinePlugin({
             'process.env': config.dev.env,
         }),
-        // HotModule插件在页面进行变更的时候只会重绘对应的页面模块，不会重绘整个页面
+        // HotModule 插件在页面进行变更的时候只会重绘对应的页面模块，不会重绘整个页面
         // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
         new webpack.HotModuleReplacementPlugin(),
         // 报错不会阻塞页面程序的运行，但是会在编译结束后提示
         new webpack.NoEmitOnErrorsPlugin(),
-        // 将index.html作为入口，注入html代码后生成index.html文件
+        // 将 index.html 作为入口，注入 html 代码后生成 index.html 文件
         // https://github.com/ampedandwired/html-webpack-plugin
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html',
+            data: { version },
             inject: true,
         }),
         new FriendlyErrorsPlugin(),
