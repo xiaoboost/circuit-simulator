64f060028dc36e51a5e653a0c48453465594521c
Xiao <xiaoboost@163.com>
2017-10-14 18:26:59 +0800

    字段名错误

--- src/lib/@types/window.d.ts
+++ src/lib/@types/window.d.ts
@@ -2,7 +2,7 @@ interface Window {
     $ENV: {
         readonly NODE_ENV: 'development' | 'production' | 'testing';
     };
-    $DATE: {
+    $DATA: {
         readonly SVG_NS: 'http://www.w3.org/2000/svg';
     };
     $DEBUG: any;
