4faeedff08312a07ba29fe2d5f2b47c80835e12a
Xiao <xiaoboost@163.com>
2017-10-14 20:06:26 +0800

    增加严格对象和广义对象的断言

--- src/lib/assertion.ts
+++ src/lib/assertion.ts
@@ -1,102 +1,154 @@
+/**
+ * 断言：输入是否是数字
+ *
+ * @param {*} x
+ * @returns {x is number}
+ */
+function isNumber(x: any): x is number {
+    return (typeof x === 'number');
+}
+
+/**
+ * 断言：输入是否是字符串
+ *
+ * @param {*} x
+ * @returns {x is string}
+ */
+function isString(x: any): x is string {
+    return (typeof x === 'string');
+}
+
+/**
+ * 断言：输入是否是布尔值
+ *
+ * @param {*} x
+ * @returns {x is boolean}
+ */
+function isBoolean(x: any): x is boolean {
+    return (typeof x === 'boolean');
+}
+
+/**
+ * 断言：输入是否是 Symbol 类型
+ *
+ * @param {*} x
+ * @returns {x is symbol}
+ */
+function isSymbol(x: any): x is symbol {
+    return (typeof x === 'symbol');
+}
+
+/**
+ * 断言：输入是否是 null 或 undefined
+ *
+ * @param {*} x
+ * @returns {(x is null | undefined)}
+ */
+function isNull(x: any): x is null | undefined {
+    const type: string = Object.prototype.toString.call(x);
+    return (type === '[object Null]' || type === '[object Undefined]');
+}
+
+/**
+ * 断言：输入是否是函数
+ *
+ * @param {*} x
+ * @returns {x is () => any}
+ */
+function isFuncton(x: any): x is () => any {
+    return (typeof x === 'function');
+}
+
+/**
+ * 断言：输入是否是严格意义上的对象
+ *  - 不包括 Array、Function、Promise 等内建类的实例以及它们继承类的实例
+ *
+ * @param {*} x
+ * @returns {x is object}
+ */
+function isStrictObject(x: any): x is object {
+    return (Object.prototype.toString.call(x) === '[object object]');
+}
+
+/**
+ * 断言：输入是否是广义上的对象
+ *  - 包括所有 Object 类的实例，以及 Object 继承类的实例
+ *
+ * @param {*} x
+ * @returns {x is object}
+ */
+function isObject(x: any): x is object {
+    const type = typeof x;
+    return (
+        (type === 'object') ||
+        (type === 'function')
+    );
+}
+
+/**
+ * 断言：输入是否是 Array 的实例（包含继承 Array 类的实例）
+ *
+ * @param {*} x
+ * @returns {x is any[]}
+ */
+function isArray(x: any): x is any[] {
+    return (Object.prototype.toString.call(x) === '[object Array]');
+}
+
+/**
+ * 断言：输入是否是正则表达式
+ *
+ * @param {*} x
+ * @returns {x is RegExp}
+ */
+function isRegExp(x: any): x is RegExp {
+    return (Object.prototype.toString.call(x) === '[object RegExp]');
+}
+
+/**
+ * 断言：输入是否是 DOM 元素
+ *
+ * @param {*} x
+ * @returns {x is HTMLElement}
+ */
+function isElement(x: any): x is HTMLElement {
+    return (/^\[object HTML([a-zA-Z]+)?Element\]$/.test(Object.prototype.toString.call(x)));
+}
+
+/**
+ * 断言：输入是否是基础类型
+ *
+ * @param {*} x
+ * @returns {(x is number | string | boolean | symbol | null | undefined)}
+ */
+function isBaseType(x: any): x is number | string | boolean | symbol | null | undefined {
+    return (!isObject(x));
+}
+
+/**
+ * 断言：输入是否是除 null 和 undefined 之外的基础类型
+ *
+ * @param {*} x
+ * @returns {(x is number | string | boolean | symbol)}
+ */
+function isBaseTypeExNull(x: any): x is number | string | boolean | symbol {
+    return (
+        isBaseType(x) && !isNull(x)
+    );
+}
+
 export default {
-    /**
-     * 断言：输入是否是数字
-     *
-     * @param {*} x
-     * @returns {x is number}
-     */
-    isNumber(x: any): x is number {
-        return (typeof x === 'number');
-    },
-
-    /**
-     * 断言：输入是否是字符串
-     *
-     * @param {*} x
-     * @returns {x is string}
-     */
-    isString(x: any): x is string {
-        return (typeof x === 'string');
-    },
-
-    /**
-     * 断言：输入是否是布尔值
-     *
-     * @param {*} x
-     * @returns {x is boolean}
-     */
-    isBoolean(x: any): x is boolean {
-        return (typeof x === 'boolean');
-    },
-
-    /**
-     * 断言：输入是否是 Symbol 类型
-     *
-     * @param {*} x
-     * @returns {x is symbol}
-     */
-    isSymbol(x: any): x is symbol {
-        return (typeof x === 'symbol');
-    },
-
-    /**
-     * 断言：输入是否是函数
-     *
-     * @param {*} x
-     * @returns {x is () => any}
-     */
-    isFuncton(x: any): x is () => any {
-        return (typeof x === 'function');
-    },
-
-    /**
-     * 断言：输入是否是 null 或 undefined
-     *
-     * @param {*} x
-     * @returns {(x is null | undefined)}
-     */
-    isNull(x: any): x is null | undefined {
-        const type: string = Object.prototype.toString.call(x);
-        return (type === '[object Null]' || type === '[object Undefined]');
-    },
-
-    /**
-     * 断言：输入是否是对象
-     *
-     * @param {*} x
-     * @returns {x is {}}
-     */
-    isObject(x: any): x is {} {
-        return (Object.prototype.toString.call(x) === '[object object]');
-    },
-
-    /**
-     * 断言：输入是否是 Array 的实例（包含继承 Array 类的实例）
-     *
-     * @param {*} x
-     * @returns {x is any[]}
-     */
-    isArray(x: any): x is any[] {
-        return (Object.prototype.toString.call(x) === '[object Array]');
-    },
-
-    /**
-     * 断言：输入是否是正则表达式
-     *
-     * @param {*} x
-     * @returns {x is RegExp}
-     */
-    isRegExp(x: any): x is RegExp {
-        return (Object.prototype.toString.call(x) === '[object RegExp]');
-    },
-
-    /**
-     * 断言：输入是否是 DOM 元素
-     *
-     * @param {*} x
-     * @returns {x is HTMLElement}
-     */
-    isElement(x: any): x is HTMLElement {
-        return (/^\[object HTML([a-zA-Z]+)?Element\]$/.test(Object.prototype.toString.call(x)));
-    },
+    isNumber,
+    isString,
+    isBoolean,
+    isSymbol,
+    isNull,
+    isBaseType,
+    isBaseTypeExNull,
+    isFuncton,
+    isObject,
+    isStrictObject,
+    isArray,
+    isRegExp,
+    isElement,
 };
