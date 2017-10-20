c2e30b5231d5acb66fb2ef6bb0e4ea2c7208b142
Xiao <xiaoboost@163.com>
2017-10-21 01:49:17 +0800

    删除原设置文件

--- config/common.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    $DATA: {
-        SVG_NS: 'http://www.w3.org/2000/svg',
-    },
-};
--- config/test.js
+++
@@ -1,8 +0,0 @@
-const devEnv = require('./dev.env');
-const common = require('./common');
-const merge = require('./merge');
-
-module.exports = merge(
-    {}, devEnv, common,
-    { $ENV: { NODE_ENV: 'testing' }}
-);
