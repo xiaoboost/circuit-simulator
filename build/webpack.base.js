8ae2085573341e33a19f1971d6bc6c273e2c496f
Xiao <xiaoboost@163.com>
2017-11-01 23:27:17 +0800

    保留原环境变量

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -105,6 +105,7 @@ module.exports = {
             },
         }),
         new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
             '$ENV.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
         }),
         new ExtractTextPlugin({
