f553d9c067b852b2796ac96553ef40c492686b52
Xiao <xiaoboost@163.com>
2017-11-02 00:04:26 +0800

    app.vue 初始化

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
 <main id="app">
-    {{ msg }}
-    <!-- <drawing-main></drawing-main>
+    <!-- <drawing-main></drawing-main> -->
     <slider-menu></slider-menu>
-    <action-menu></action-menu> -->
+    <action-menu></action-menu>
 </main>
 </template>
 
 <script lang="ts">
-import * as deco from 'vue-property-decorator';
-
-// import ActionMenu from 'src/components/action-menu';
-// import SliderMenu from 'src/components/slider-menu';
+import Vue from 'vue';
+import ActionMenu from 'src/components/action-menu';
+import SliderMenu from 'src/components/slider-menu';
 // import DrawingMain from 'src/components/drawing-main';
 
-@deco.Component
-export default class Main extends deco.Vue {
-    // components: {
-    //     ActionMenu,
-    //     SliderMenu,
-    //     DrawingMain,
-    // };
-    msg = '初始页面';
-};
+export default Vue.extend({
+    name: 'Main',
+    components: {
+        ActionMenu,
+        SliderMenu,
+        // DrawingMain,
+    },
+});
 </script>
