4d16d71abd9cd061f10c6a7bceef98cdae9096a7
Xiao <xiaoboost@163.com>
2017-11-04 00:10:45 +0800

    添加全局 EventExtend 接口

--- src/lib/types/window.d.ts
+++ src/lib/types/window.d.ts
@@ -2,4 +2,10 @@ interface Environment {
     readonly NODE_ENV: 'development' | 'production' | 'testing';
 }
 
+interface Extend {
+    target: HTMLElement & EventTarget;
+    currentTarget: HTMLElement & EventTarget;
+}
+
 declare const $ENV: Environment;
+declare type EventExtend = Extend;
