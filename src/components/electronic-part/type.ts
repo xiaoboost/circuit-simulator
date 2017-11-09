ae897d90d1e0b324ab166024a17152b3fee113a4
Xiao <xiaoboost@163.com>
2017-11-09 22:50:16 +0800

    器件导线组件对外数据接口初始化

--- src/components/electronic-part/type.ts
+++ src/components/electronic-part/type.ts
@@ -1,3 +1,4 @@
+import Vue from 'vue';
 import { Point } from 'src/lib/point';
 import { Matrix } from 'src/lib/matrix';
 
@@ -9,4 +10,17 @@ export interface PartData {
     position: Point;
     params: string[];
     connect: string[];
+}
+
+/** 器件引脚描述接口 */
+export interface PointClass {
+    position: Point;
+    direction: Point;
+    class: 'part-point-close' | 'part-point-open';
+}
+
+/** 器件组件对外接口 */
+export interface PartComponent extends Vue {
+    /** 器件 ID 编号 */
+    id: string;
 }
