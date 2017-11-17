1fa7645c5f662a57d247db5a87a31c66af1c2573
Xiao <xiaoboost@163.com>
2017-11-17 22:18:48 +0800

    图纸组件添加部分接口说明

--- src/components/drawing-main/index.ts
+++ src/components/drawing-main/index.ts
@@ -1,2 +1,18 @@
 import main from './index.vue';
+import { Point } from 'src/lib/point';
+
+import { DrawEventSetting } from './events';
+import { PartComponent } from 'src/components/electronic-part';
+import { LineComponent } from 'src/components/electronic-line';
+
 export default main;
+export * from './events';
+
+export interface MapStatus {
+    zoom: number;
+    position: Point;
+}
+
+export type FindPart = (id: string | HTMLElement | { id: string }) => PartComponent;
+export type FindLine = (id: string | HTMLElement | { id: string }) => LineComponent;
+export type SetDrawEvent = (handlers: DrawEventSetting) => void;
