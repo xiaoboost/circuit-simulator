f4929e6e9fea4a2be1a3fdc6224d519f86ebf845
Xiao <xiaoboost@163.com>
2017-11-18 22:13:27 +0800

    修正 required 验证，添加 placeholder 样式

--- src/components/input-verifiable/index.vue
+++ src/components/input-verifiable/index.vue
@@ -10,6 +10,7 @@
 </template>
 
 <script lang="ts">
+import * as assert from 'src/lib/assertion';
 import { Component, Vue, Prop } from 'vue-property-decorator';
 
 @Component
@@ -43,27 +44,29 @@ export default class InputVerifiable extends Vue {
         if (value.length <= this.maxlength) {
             this.$emit('input', value);
         }
-    };
-    check(value: string): boolean {
-        value = value || this.value;
+    }
+    check(value?: string): boolean {
+        const input = assert.isString(value) ? value : this.value;
+
         this.isError = false;
-        if (this.required && !value) {
+
+        if (this.required && !input) {
             this.$emit('error', 'required');
             this.isError = true;
             return (false);
         }
-        if (!this.pattern.test(value)) {
+        if (!this.pattern.test(input)) {
             this.$emit('error', 'pattern');
             this.isError = true;
             return (false);
         }
-        if (!this.func(value)) {
+        if (!this.func(input)) {
             this.$emit('error', 'function');
             this.isError = true;
             return (false);
         }
         return (true);
-    };
+    }
 }
 </script>
 
@@ -75,7 +78,7 @@ export default class InputVerifiable extends Vue {
 
 input
     width 100%
-    font-size 1.1em
+    font-size inherit
     outline 0
     border-bottom 1px solid Gray
     background-color White
@@ -89,7 +92,7 @@ input
     content ''
     height 2px
     width 0
-    bottom 2px
+    bottom 4px
     left 0
     position absolute
     background Dark-Blue
@@ -107,4 +110,20 @@ input:focus ~ .correct-bar:before
 .input-error-message
     position absolute
     padding 0 5px
+
+input::-webkit-input-placeholder
+    color: Shadow
+    font-size: 85%
+
+input::-ms-input-placeholder
+    color: Shadow
+    font-size: 85%
+
+input::-moz-placeholder
+    color: Shadow
+    font-size: 85%
+
+input::-moz-placeholder
+    color: Shadow
+    font-size: 85%
 </style>
