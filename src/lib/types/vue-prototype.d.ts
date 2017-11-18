b37f1cdcb9b4a5c9cdaa6ea5988e1730b89ab67c
Xiao <xiaoboost@163.com>
2017-11-18 22:14:25 +0800

    变更参数对话框说明

--- src/lib/types/vue-prototype.d.ts
+++ src/lib/types/vue-prototype.d.ts
@@ -18,13 +18,14 @@ declare module 'vue/types/vue' {
 
         /**
          * 打开器件的参数设置对话框
+         *  - `position`是器件中心相对于屏幕左上角的坐标
          *  - 返回`Promise<void>`表示点击了取消按钮
          *  - 返回`Promise<{ id: string; params: string[] }>`表示点击了确定按钮，其中数据即为最后对话框中输入的数据
          *
          * @param {ParamsInput} { id, type, params, position }
          * @returns {(Promise<{ id: string; params: string[] } | void>)}
          */
-        setPartParams({ id, params, position }: ParamsInput): Promise<{ id: string; params: string[] } | void>
+        setPartParams({ id, type, params, position }: ParamsInput): Promise<{ id: string; params: string[] } | void>
 
         /**
          * 给`el`元素绑定事件
