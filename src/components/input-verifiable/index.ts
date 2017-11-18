750fb020a5463e52137a2760debcffc05bf61ec5
Xiao <xiaoboost@163.com>
2017-11-19 00:46:43 +0800

    输入验证添加对外接口

--- src/components/input-verifiable/index.ts
+++ src/components/input-verifiable/index.ts
@@ -1,2 +1,12 @@
+import Vue from 'vue';
 import main from './index.vue';
+
 export default main;
+
+/** 验证输入组件对外接口 */
+export interface InputVerifiable extends Vue {
+    /** 当前是否发生错误 */
+    isError: boolean;
+    /** 主动验证 */
+    check(): boolean;
+}
