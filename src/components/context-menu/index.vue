dc84b119c30d47a0f1f926d69f2c189d89fc7812
Xiao <xiaoboost@163.com>
2017-11-24 20:59:08 +0800

    [menu] right menu init

---
+++ src/components/context-menu/index.vue
@@ -0,0 +1,18 @@
+<template>
+<section>
+    右键菜单
+</section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ContextMenu extends Vue {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
