3cc2fbeabc27af12febf3d2650a28dfc117f4d45
Xiao <xiaoboost@163.com>
2018-03-28 22:19:35 +0800

    update typescript, global type operation.

--- types/type-operation.d.ts
+++ types/type-operation.d.ts
@@ -1,31 +1,8 @@
-/**
- * @see https://github.com/Microsoft/TypeScript/issues/12215
- * @see https://github.com/Microsoft/TypeScript/issues/14829
- * @see https://github.com/Microsoft/TypeScript/issues/15012
- */
-
-/** Remove the variants of the second union of string literals from the first. */
-declare type Diff<T extends string, U extends string> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T];
+/** Ignoring some properties in an interface */
+declare type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 /** To overwrite a read-only interface as writable */
-declare type Writeable<T extends { [x: string]: any }, K extends string> = Pick<T, Diff<keyof T, K>> & { [P in K]: T[P] };
-
-/** Ignoring some properties in an interface */
-declare type Omit<T, K extends keyof T> = { [P in Diff<keyof T, K>]: T[P] };
+declare type Writeable<T extends object, K extends keyof T> = Omit<T, K> & { -readonly [P in K]: T[P] };
 
 /** Overwrite some property types in an interface */
-declare type Overwrite<T, U> = { [P in Diff<keyof T, keyof U>]: T[P] } & U;
-
-/** Use to prevent a usage of type `T` from being inferred in other generics. */
-declare type NoInfer<T> = T & { [K in keyof T]: T[K] };
-
-/** `T` without the possibility of `undefined` or `null`. */
-declare type NonNullable<T> = T & {};
-
-/** Forgets contextual of partiality of keys. */
-declare type Purify<T extends string> = { [P in T]: T; }[T];
-
-/** Make all properties of `T` required and non-nullable. */
-declare type Required<T> = {
-    [P in Purify<keyof T>]: NonNullable<T[P]>;
-};
+declare type Overwrite<T extends object, K extends object> = Omit<T, Extract<keyof T, keyof K>> & K;
