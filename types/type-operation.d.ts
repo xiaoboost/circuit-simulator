09a110b6a1690bc16aad1df591d8e8894bd62664
Xiao <xiaoboost@qq.com>
2018-10-05 22:58:12 +0800

    delete Parameters and add GetArray

--- types/type-operation.d.ts
+++ types/type-operation.d.ts
@@ -7,5 +7,5 @@ declare type Writeable<T extends object, K extends keyof T> = Omit<T, K> & { -re
 /** Overwrite some property types in an interface */
 declare type Overwrite<T extends object, K extends object> = Omit<T, Extract<keyof T, keyof K>> & K;
 
-/** return parameters type of function */
-declare type Parameters<T extends Function> = T extends (...args: infer R) => any ? R : never;
+/** get array type in union type */
+declare type GetArray<T> = T extends (any | (infer R)[]) ? R[] : never;
