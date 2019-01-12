28069da99c35b83ab991c0955bb4ff52021fbed8
xiaoboost <xiaoboost@qq.com>
2019-01-12 15:52:42 +0800

    fix ts opration

--- types/type-operation.d.ts
+++ types/type-operation.d.ts
@@ -1,11 +1,19 @@
 /** Ignoring some properties in an interface */
-declare type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+declare type Omit<T, K extends keyof T> = { [key in Exclude<keyof T, K>]: T[key] };
+
+declare type PartPartial<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
 /** To overwrite a read-only interface as writable */
-declare type Writeable<T extends object, K extends keyof T> = Omit<T, K> & { -readonly [P in K]: T[P] };
+declare type Writeable<T extends object> = { -readonly [P in keyof T]: T[P] };
 
 /** Overwrite some property types in an interface */
 declare type Overwrite<T extends object, K extends object> = Omit<T, Extract<keyof T, keyof K>> & K;
 
-/** get array type in union type */
+/** return the array type */
 declare type GetArray<T> = T extends (any | (infer R)[]) ? R[] : never;
+
+/** return the array type */
+declare type GetArrayItem<T> = T extends (infer R)[] ? R : never;
+
+/** any object */
+declare type AnyObject<T = any> = { [key: string]: T };
