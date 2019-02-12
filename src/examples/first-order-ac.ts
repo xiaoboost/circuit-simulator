0fa8f818513643498d31705b8e0ed923c17597dc
Xiao <xiaoboost@qq.com>
2019-02-12 20:22:47 +0800

    change example files to .ts file

---
+++ src/examples/first-order-ac.ts
@@ -0,0 +1,98 @@
+import { CircuitStorage } from '../vuex';
+import { PartType } from '../components/electronic-part/parts';
+
+export const data: CircuitStorage = {
+    data: [
+        {
+            type: PartType.Resistance,
+            id: 'R_1',
+            position: [340, 200],
+            rotate: [[1, 0], [0, 1]],
+            params: ['200'],
+        },
+        {
+            type: PartType.Capacitor,
+            id: 'C_1',
+            position: [420, 280],
+            rotate: [[0, 1], [-1, 0]],
+            params: ['10u'],
+        },
+        {
+            type: PartType.ReferenceGround,
+            id: 'GND_1',
+            position: [340, 400],
+            rotate: [[1, 0], [0, 1]],
+        },
+        {
+            type: PartType.VoltageMeter,
+            id: 'V_in',
+            position: [260, 280],
+            rotate: [[1, 0], [0, 1]],
+        },
+        {
+            type: PartType.VoltageMeter,
+            id: 'V_C1',
+            position: [520, 280],
+            rotate: [[1, 0], [0, 1]],
+        },
+        {
+            type: PartType.AcVoltageSource,
+            id: 'V_1',
+            position: [160, 280],
+            rotate: [[1, 0], [0, 1]],
+            params: ['20', '100', '0', '0'],
+        },
+        {
+            type: -1,
+            way: [[380, 200], [420, 200]],
+        },
+        {
+            type: -1,
+            way: [[340, 380], [340, 360]],
+        },
+        {
+            type: -1,
+            way: [[340, 360], [420, 360]],
+        },
+        {
+            type: -1,
+            way: [[520, 240], [520, 200], [420, 200]],
+        },
+        {
+            type: -1,
+            way: [[420, 240], [420, 200]],
+        },
+        {
+            type: -1,
+            way: [[520, 320], [520, 360], [420, 360]],
+        },
+        {
+            type: -1,
+            way: [[420, 320], [420, 360]],
+        },
+        {
+            type: -1,
+            way: [[260, 240], [260, 200]],
+        },
+        {
+            type: -1,
+            way: [[260, 320], [260, 360]],
+        },
+        {
+            type: -1,
+            way: [[160, 240], [160, 200], [260, 200]],
+        },
+        {
+            type: -1,
+            way: [[260, 200], [300, 200]],
+        },
+        {
+            type: -1,
+            way: [[160, 320], [160, 360], [260, 360]],
+        },
+        {
+            type: -1,
+            way: [[260, 360], [340, 360]],
+        },
+    ],
+};
---
+++ src/examples/half-wave-rectifier.ts
@@ -0,0 +1,87 @@
+import { CircuitStorage } from '../vuex';
+import { PartType } from '../components/electronic-part/parts';
+
+export const data: CircuitStorage = {
+    data: [
+        {
+            type: PartType.AcVoltageSource,
+            id: 'V_1',
+            position: [180, 220],
+            rotate: [[1, 0], [0, 1]],
+            params: ['20', '200', '0', '0'],
+        },
+        {
+            type: PartType.Diode,
+            id: 'VD_1',
+            position: [280, 140],
+            rotate: [[0, 1], [-1, 0]],
+            params: ['1', '0.5', '0.2G'],
+        },
+        {
+            type: PartType.Resistance,
+            id: 'R_1',
+            position: [460, 220],
+            rotate: [[0, 1], [-1, 0]],
+            params: ['100'],
+        },
+        {
+            type: -1,
+            way: [[180, 180], [180, 140], [240, 140]],
+        },
+        {
+            type: -1,
+            way: [[320, 140], [380, 140]],
+        },
+        {
+            type: -1,
+            way: [[420, 140], [460, 140]],
+        },
+        {
+            type: -1,
+            way: [[520, 180], [520, 140], [460, 140]],
+        },
+        {
+            type: -1,
+            way: [[460, 140], [460, 180]],
+        },
+        {
+            type: -1,
+            way: [[180, 260], [180, 300], [320, 300]],
+        },
+        {
+            type: PartType.ReferenceGround,
+            id: 'GND_1',
+            position: [320, 340],
+            rotate: [[1, 0], [0, 1]],
+        },
+        {
+            type: -1,
+            way: [[320, 320], [320, 300]],
+        },
+        {
+            type: -1,
+            way: [[320, 300], [460, 300]],
+        },
+        {
+            type: -1,
+            way: [[520, 260], [520, 300], [460, 300]],
+        },
+        {
+            type: -1,
+            way: [[460, 300], [460, 260]],
+        },
+        {
+            type: PartType.CurrentMeter,
+            id: 'I_out',
+            position: [400, 140],
+            rotate: [[1, 0], [0, 1]],
+        },
+        {
+            type: PartType.VoltageMeter,
+            id: 'V_out',
+            position: [520, 220],
+            rotate: [[1, 0], [0, 1]],
+            text: 'right',
+        },
+    ],
+};
