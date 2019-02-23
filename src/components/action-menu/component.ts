8d530c798a5ab24148a16c42e0332ffcd6ec32a1
Xiao <xiaoboost@qq.com>
2019-02-23 16:49:24 +0800

    fix action icons

--- src/components/action-menu/component.ts
+++ src/components/action-menu/component.ts
@@ -1,14 +1,7 @@
-import { actions } from './icons';
 import { Getter } from 'vuex-class';
 import { Component, Vue } from 'vue-property-decorator';
 import { Mutation, Action } from 'src/vuex';
-
-type IconKey = keyof typeof actions;
-const iconkeys: IconKey[] = ['run', 'add', 'config'];
-const icons = iconkeys.map((item) => ({
-    name: item,
-    ...actions[item],
-}));
+import { icons, IconName } from './icons';
 
 @Component
 export default class ActionMenu extends Vue {
@@ -23,15 +16,15 @@ export default class ActionMenu extends Vue {
     @Getter('isSpace')
     vision!: boolean;
 
-    action(name: IconKey) {
+    action(name: IconName) {
         switch (name) {
-            case 'add':
+            case IconName.Add:
                 this.$store.commit(Mutation.OPEN_ADD_PARTS);
                 break;
-            case 'config':
+            case IconName.Config:
                 this.$store.commit(Mutation.OPEN_MAIN_CONFIG);
                 break;
-            case 'run':
+            case IconName.Run:
                 this.isRun = true;
                 this.$store.dispatch(Action.SOLVE_CIRCUIT);
                 break;
