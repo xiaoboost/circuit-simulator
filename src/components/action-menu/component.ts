830cc5df0a2cad8e5d8935b8148102ce38088d1b
Xiao <xiaoboost@qq.com>
2019-02-26 01:25:29 +0800

    add vuex type

--- src/components/action-menu/component.ts
+++ src/components/action-menu/component.ts
@@ -1,7 +1,15 @@
-import { Getter } from 'vuex-class';
-import { Component, Vue } from 'vue-property-decorator';
-import { Mutation, Action } from 'src/vuex';
 import { icons, IconName } from './icons';
+import { Component, Vue } from 'vue-property-decorator';
+import { Getter, Mutation, Action } from 'vuex-class';
+
+import {
+    GetterName,
+    Getter as GetterTree,
+    MutationName,
+    Mutation as MutationTree,
+    ActionName,
+    Action as ActionTree,
+} from 'src/vuex';
 
 @Component
 export default class ActionMenu extends Vue {
@@ -13,20 +21,32 @@ export default class ActionMenu extends Vue {
     private isRun = false;
 
     /** 是否显示菜单 */
-    @Getter('isSpace')
-    vision!: boolean;
+    @Getter(GetterName.isSpace)
+    vision!: GetterTree[GetterName.isSpace];
+
+    /** 打开添加器件侧边栏 */
+    @Mutation(MutationName.OPEN_ADD_PARTS)
+    addParts!: MutationTree[MutationName.OPEN_ADD_PARTS];
+
+    /** 打开主配置侧边栏 */
+    @Mutation(MutationName.OPEN_MAIN_CONFIG)
+    setConfig!: MutationTree[MutationName.OPEN_MAIN_CONFIG];
+
+    /** 运行电路模拟 */
+    @Action(ActionName.SOLVE_CIRCUIT)
+    simulate!: ActionTree[ActionName.SOLVE_CIRCUIT];
 
     action(name: IconName) {
         switch (name) {
             case IconName.Add:
-                this.$store.commit(Mutation.OPEN_ADD_PARTS);
+                this.addParts();
                 break;
             case IconName.Config:
-                this.$store.commit(Mutation.OPEN_MAIN_CONFIG);
+                this.setConfig();
                 break;
             case IconName.Run:
                 this.isRun = true;
-                this.$store.dispatch(Action.SOLVE_CIRCUIT);
+                this.simulate();
                 break;
             default:
         }
--- src/vuex/constant.ts
+++ src/vuex/constant.ts
@@ -8,30 +8,6 @@ export interface TimeConfig {
     step: string;
 }
 
-/** vuex 状态接口 */
-export interface StateType {
-    /**
-     * 侧边栏状态
-     */
-    sidebar: Sidebar;
-    /**
-     * 全局时间设置
-     */
-    time: TimeConfig;
-    /**
-     * 全局器件堆栈
-     */
-    parts: PartData[];
-    /**
-     * 全局导线堆栈
-     */
-    lines: LineData[];
-    /**
-     * 历史数据
-     */
-    historyData: Array<Array<PartData | LineData>>;
-}
-
 /** 储存用的器件数据接口 */
 export interface PartStorageData {
     type: PartType;
@@ -57,13 +33,54 @@ export interface SimulateConfig extends TimeConfig {
     // TODO: 波形输出标志
 }
 
+/** 电路数据 */
 export interface CircuitStorage {
     config?: SimulateConfig;
     data: ElectronicsData;
 }
 
+/** 侧边栏状态 */
+export const enum Sidebar {
+    Space,
+    Parts,
+    Config,
+    Graph,
+}
+
+/** vuex 状态接口 */
+export interface State {
+    /**
+     * 侧边栏状态
+     */
+    sidebar: Sidebar;
+    /**
+     * 全局时间设置
+     */
+    time: TimeConfig;
+    /**
+     * 全局器件堆栈
+     */
+    parts: PartData[];
+    /**
+     * 全局导线堆栈
+     */
+    lines: LineData[];
+    /**
+     * 历史数据
+     */
+    historyData: Array<Array<PartData | LineData>>;
+}
+
+/** Getter 键名 */
+export const enum GetterName {
+    isSpace = 'isSpace',
+    showPartsPanel = 'showPartsPanel',
+    showConfigPanel = 'showConfigPanel',
+    showGraphView = 'showGraphView',
+}
+
 /** Mutation 键名 */
-export const enum Mutation {
+export const enum MutationName {
     /** 关闭侧边栏 */
     CLOSE_SLIDER = 'CLOSE_SLIDER',
     /** 打开添加器件侧边栏 */
@@ -97,17 +114,65 @@ export const enum Mutation {
 }
 
 /** Action 键名 */
-export const enum Action {
+export const enum ActionName {
     /** 外部数据导入 */
     IMPORT_DATA = 'IMPORT_DATA',
     /** 求解电路 */
     SOLVE_CIRCUIT = 'SOLVE_CIRCUIT',
 }
 
-/** 侧边栏状态 */
-export const enum Sidebar {
-    Space,
-    Parts,
-    Config,
-    Graph,
+/** Getter 原型定义 */
+export interface Getter {
+    [GetterName.isSpace]: boolean;
+    [GetterName.showPartsPanel]: boolean;
+    [GetterName.showConfigPanel]: boolean;
+    [GetterName.showGraphView]: boolean;
+}
+
+/** Mutation 原型定义 */
+export interface Mutation {
+    /** 关闭侧边栏 */
+    [MutationName.CLOSE_SLIDER](): void;
+    /** 打开添加器件侧边栏 */
+    [MutationName.OPEN_ADD_PARTS](): void;
+    /** 打开总设置侧边栏 */
+    [MutationName.OPEN_MAIN_CONFIG](): void;
+    /** 打开波形界面 */
+    [MutationName.OPEN_GRAPH_VIEW](): void;
+
+    /** 设置时间 */
+    [MutationName.SET_TIME_CONFIG](time: TimeConfig): void;
+
+    /** 新器件压栈 */
+    [MutationName.PUSH_PART](data: PartData | PartData[]): void;
+    /** 更新器件数据 */
+    [MutationName.UPDATE_PART](data: PartData): void;
+    /** 复制器件 */
+    [MutationName.COPY_PART](IDs: string[]): void;
+
+    /** 新导线压栈 */
+    [MutationName.PUSH_LINE](data: LineData | LineData[]): void;
+    /** 更新导线数据 */
+    [MutationName.UPDATE_LINE](data: LineData): void;
+    /** 复制导线 */
+    [MutationName.COPY_LINE](IDs: string[]): void;
+
+    /** 删除器件与导线 */
+    [MutationName.DELETE_ELEC](eles: string | string[]): void;
+
+    /** 导线放置到底层 */
+    [MutationName.LINE_TO_BOTTOM](id: string): void;
+
+    /** 记录当前数据 */
+    [MutationName.RECORD_MAP](): void;
+    /** 图纸数据回滚 */
+    [MutationName.HISTORY_BACK](): void;
+}
+
+/** Action 原型定义 */
+export interface Action {
+    /** 外部数据导入 */
+    [ActionName.IMPORT_DATA](data: CircuitStorage): Promise<void>;
+    /** 求解电路 */
+    [ActionName.SOLVE_CIRCUIT](): Promise<void>;
 }
