1ffb3c3e44046865e9a1f95f96a89173f5e398b4
xiaoboost <xiaoboost@qq.com>
2021-05-30 23:24:24 +0800

    fix data store

--- packages/website/src/components/container/utils.ts
+++ packages/website/src/components/container/utils.ts
@@ -1,7 +1,17 @@
 import { parse } from 'qs';
 import { useEffect } from 'react';
-import { load } from 'src/store';
 import { delay } from '@xiao-ai/utils';
+import { local } from '@xiao-ai/utils/web';
+import { CircuitDataKey } from 'src/lib/constant';
+import { SimulationConfig } from 'src/components/side-menu';
+import { PartData, LineData, Part, Line, dispatch } from 'src/components/electronics';
+
+/** 电路数据 */
+export interface CircuitData {
+  time?: SimulationConfig;
+  oscilloscopes?: string[][];
+  electronics?: (PartData | LineData)[];
+}
 
 /** 移除 loading 界面 */
 function removeLoading() {
@@ -17,19 +27,41 @@ function removeLoading() {
 async function fetchMapData() {
   const map = parse(location.search.slice(1)).map;
 
+  let data: CircuitData | null | undefined;
+
   if (map) {
     const response = await import(`src/examples/${map}.ts`).catch((e) => {
       console.error(e);
     });
 
-    if (!response) {
-      return;
+    if (response) {
+      data = response.data;
     }
+  }
+  else {
+    data = local.get(CircuitDataKey);
+  }
 
-    // 加载数据
-    await load(response.data);
+  // 加载数据
+  if (data) {
+    await load(data);
     await delay();
   }
+}
+
+/** 加载数据 */
+async function load(data: CircuitData) {
+  // 加载期间
+  for (const item of (data.electronics ?? [])) {
+    if (item.kind === 'Line') {
+      // ..
+    }
+    else {
+      new Part(item as PartData).setSign();
+    }
+  }
+  
+  dispatch();
 }
 
 /** 初始化 */
