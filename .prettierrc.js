a55efa3baec40276be497180cc76ce8dc9307965
xiaoboost <xiaoboost@qq.com>
2024-04-27 22:15:05 +0800

    chore: update version and config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSameLine: false,
+  endOfLine: 'crlf',
+};
---
+++ packages/electronics/.eslintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: [require.resolve('@xiao-ai/eslint-config')],
+};
