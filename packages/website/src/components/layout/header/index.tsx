68308127e4a53d2e5b00a23a0116f5eb06bc7cd0
xiaoboost <xiaoboost@qq.com>
2025-06-08 18:14:00 +0800

    feature: finish logo img

--- packages/website/src/components/layout/header/index.tsx
+++ packages/website/src/components/layout/header/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import Logo from './assets/logo.png';
+
 import * as Styles from './styles.less';
 
 export function Header() {
-  return <div className={Styles.header}>
-    测试
-  </div>;
+  return (
+    <div className={Styles.header}>
+      <span className={Styles.title}>
+        <img src={Logo} alt="pcb" />
+        <span className={Styles.titleText}>电路图绘制·仿真</span>
+      </span>
+    </div>
+  );
 }
--- tools/typings/files.d.ts
+++ tools/typings/files.d.ts
@@ -3,6 +3,11 @@ declare module '*.svg' {
   export default content;
 }
 
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
 declare module '*.less' {
   const classNames: Record<string, string>;
   export = classNames;
