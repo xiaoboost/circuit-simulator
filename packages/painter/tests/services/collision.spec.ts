d7ac37e7bfd31dfda4778c39a9888760758019b2
xiaoboost <xiaoboost@qq.com>
2025-08-02 17:38:32 +0800

    fix: add collision text

--- packages/painter/tests/services/collision.spec.ts
+++ packages/painter/tests/services/collision.spec.ts
@@ -1,7 +1,7 @@
 import { Point, Direction, RotateMatrixSet, Rotate } from '@circuit/algorithm';
 import { PartStructuredData, LineStructuredData, ElectronicKind } from '@circuit/types';
 import { describe, it, expect } from 'vitest';
-import { COLLISION_SERVICE } from '../../src/types';
+import { COLLISION_SERVICE, EntityKind } from '../../src/types';
 import { registerPlugin, getPlugin } from '../utils';
 
 describe('碰撞服务', () => {
@@ -65,6 +65,35 @@ describe('碰撞服务', () => {
       { x: -15, y: -32, width: 30, height: 64 },
       { x: -8, y: -48, width: 16, height: 16 },
       { x: -8, y: 32, width: 16, height: 16 },
+    ]);
+  });
+
+  it('获取点覆盖信息', async () => {
+    const collision = await getPlugin(COLLISION_SERVICE);
+    const part: PartStructuredData = {
+      id: 'R_1',
+      kind: ElectronicKind.Resistance,
+      position: Point.from(0),
+      textDirection: Direction.Top,
+      propertyValues: [],
+      rotate: RotateMatrixSet[Rotate.Same],
+    };
+    const line: LineStructuredData = {
+      id: 'L_1',
+      path: [
+        Point.from([40, 0]),
+        Point.from([40, 100]),
+        Point.from([100, 100]),
+      ],
+    };
+
+    collision.setEntity(part);
+    collision.setEntity(line);
+
+    expect(collision.pointInEntities(Point.from([40, 0]))).toEqual([
+      { kind: EntityKind.PartPin, id: 'R_1', pin: 1 },
+      { kind: EntityKind.Line, id: 'L_1', index: 0 },
+      { kind: EntityKind.LinePin, id: 'L_1', pin: 0 },
     ]);
   });
 });
