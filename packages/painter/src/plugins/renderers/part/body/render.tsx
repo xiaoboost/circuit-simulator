5fef982dd0e2066c36a5e41ab323ef3c2e59ea66
xiaoboost <xiaoboost@qq.com>
2025-08-08 00:57:36 +0800

    chore: fix roadmap

--- packages/painter/src/plugins/renderers/part/body/render.tsx
+++ packages/painter/src/plugins/renderers/part/body/render.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import { IPartRendererProps } from '../../../../types';
 
 function PartBodyRender({ prototype }: IPartRendererProps) {
-  return (
-    <>
-      {prototype.shape.map(({ name: Tag, attribute }, index) => (
-        <Tag key={index} {...attribute} />
-      ))}
-    </>
-  );
+  return prototype.shape.map(({ name: Tag, attribute }, index) => (
+    <Tag key={index} {...attribute} />
+  ));
 }
 
 export const Render = React.memo(PartBodyRender, ({ data: prev }, { data: next }) => {
